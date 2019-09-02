51cbd9f1f02cbc0229bcdc5c8fa1c4b1c4573f0e
pathdll <pcampaa@gmail.com>
2019-09-02 16:48:02 -0300

    new signup test

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,8 @@
 import json
 
-import pytest
 import requests
-import time
-
-from nameko.testing.services import worker_factory
-from websocket import create_connection, WebSocketTimeoutException
+import random
+import string
 
 USER = lambda x: f"TestUser{x}"
 PASS = lambda x: f"secret{x}"
@@ -13,23 +10,49 @@ PASS = lambda x: f"secret{x}"
 TEST_MESSAGE = "Hello, World!"
 
 
+def random_username(length=20):
+    chars = string.ascii_letters + string.digits
+    return ''.join([random.choice(chars) for _ in range(length)])
+
+
 def test_signup():
     # signup with users
-    for user_tag in ["A", "B", "C"]:
-        url = 'http://localhost:8000/signup'
+    url = 'http://localhost:8000/signup'
+
+    for _ in range(3):
+        username = random_username()
 
         payload = {
-            'username': USER(user_tag),
-            'password': PASS(user_tag)
+            'username': username,
+            'password': "secret"
         }
 
         req = requests.post(url, json=payload)
-        print(req.content)
         data = json.loads(req.content)
 
         assert req.status_code == 200
-        assert data["user"]["username"] == USER(user_tag)
+        assert data["user"]["username"] == username
+
+    # test users
+    payload_a = {
+        'username': USER("A"),
+        'password': "secret"
+    }
+
+    payload_b = {
+        'username': USER("B"),
+        'password': "secret"
+    }
+
+    payload_c = {
+        'username': USER("C"),
+        'password': "secret"
+    }
+
+    requests.post(url, json=payload_a)
+    requests.post(url, json=payload_b)
+    requests.post(url, json=payload_c)
 
     # signup with used username
-    req = requests.post(url, json=payload)
+    req = requests.post(url, json=payload_a)
     assert req.status_code == 500

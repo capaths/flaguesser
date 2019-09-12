8f483b711a6ad7a3ae097810df14180cbedc363a
pathdll <pcampaa@gmail.com>
2019-09-12 01:43:54 -0300

    Added webapp to deployment

--- src/webapp/webapp/run.py
+++ src/webapp/webapp/run.py
@@ -3,7 +3,8 @@ import os
 from app import SocketApp
 
 PROD = os.environ.get("ENV") == "prod"
+PORT = os.environ.get("PORT", 8080)
 
 if __name__ == "__main__":
     app = SocketApp(for_production=PROD)
-    app.run(host='0.0.0.0', port=8080 if PROD else 8081)
+    app.run(host='0.0.0.0', port=PORT if PROD else 8081)

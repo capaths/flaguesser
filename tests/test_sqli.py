074ba088a4463a4f076af129cb9e325126adc839
161905 <cristobal.carmona.14@sansano.usm.cl>
2019-09-12 11:10:11 -0300

    SQL Injection Test 4

--- tests/test_sqli.py
+++ tests/test_sqli.py
@@ -7,10 +7,10 @@ SLQ_Injection = "Chile" + "'" + "); DROP TABLE *;"
 
 def test_sqli():
     req = requests.post("http://localhost:8000/signup", json = {
-        "username": "SQL Injection Test",
+        "username": "SQL_Injection_Test",
         "password": "password",
         "country": SLQ_Injection
     })
 
-    print(req.content)
-    assert False
+    req = requests.get("http://localhost:8000/player/SQL_Injection_Test")
+    assert req.status == 200
